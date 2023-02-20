ba2c2a8fe57f48d7c7edd700917e377cc7fad860
silviochivo <esechivin@hotmail.com>
2023-02-20 11:44:36 +0100

    Ajout des Projets + Filtres Fonctionnels

--- FrontEnd/works.js
+++ FrontEnd/works.js
@@ -33,30 +33,66 @@ async function loadProjects() {
     }
 };
 
+
 loadProjects();
 
 /*Filtres */
+
 async function filterProjects() {
     const response = await fetch("http://localhost:5678/api/works");
     const filterWorks = await response.json();
-
+  
+    function afficherProjetsFiltres(works) {
+      let sectionProjet = document.querySelector(".gallery");
+      sectionProjet.innerHTML = "";
+  
+      for (let i = 0; i < works.length; i++) {
+        let boxWork = works[i];
+        let figureElement = document.createElement("figure");
+  
+        let imageElement = document.createElement("img");
+        imageElement.src = works[i].imageUrl;
+        imageElement.alt = works[i].title;
+  
+        let figureCaptionElement = document.createElement("figcaption");
+        figureCaptionElement.innerHTML = works[i].title;
+  
+        figureElement.appendChild(imageElement);
+        figureElement.appendChild(figureCaptionElement);
+        sectionProjet.appendChild(figureElement);
+      }
+    }
+  
     const filterButtonObject = document.getElementById('Boutton-Filter-Objets');
     filterButtonObject.addEventListener('click', function() {
-    const WorkFilteredObjects = filterWorks.filter(works => works.categoryId == 1);
-    console.log(WorkFilteredObjects);
-});
-
+      const WorkFilteredObjects = filterWorks.filter(works => works.categoryId == 1);
+      console.log(WorkFilteredObjects);
+      afficherProjetsFiltres(WorkFilteredObjects);
+    });
+  
     const filterButtonAppartements = document.getElementById('Boutton-Filter-Appartements');
     filterButtonAppartements.addEventListener('click', function() {
-    const WorkFilteredAppartements = filterWorks.filter(works => works.categoryId == 2);
-    console.log(WorkFilteredAppartements);
-});
+      const WorkFilteredAppartements = filterWorks.filter(works => works.categoryId == 2);
+      console.log(WorkFilteredAppartements);
+      afficherProjetsFiltres(WorkFilteredAppartements);
+    });
+  
     const filterButtonHotels = document.getElementById('Boutton-Filter-Hotels-Et-Restaurants');
     filterButtonHotels.addEventListener('click', function() {
-    const WorkFilteredHotels = filterWorks.filter(works => works.categoryId == 3);
-    console.log(WorkFilteredHotels);
-});
+      const WorkFilteredHotels = filterWorks.filter(works => works.categoryId == 3);
+      console.log(WorkFilteredHotels);
+      afficherProjetsFiltres(WorkFilteredHotels);
+    });
 
-}
-filterProjects();
+    const filterButtonTous = document.getElementById('Boutton-Filter-Tous');
+    filterButtonTous.addEventListener('click', function() {
+        afficherProjetsFiltres(filterWorks);
+    });
+  
+    // Affiche tous les projets par défaut
+    afficherProjetsFiltres(filterWorks);
 
+  }
+  
+  filterProjects();
+  
